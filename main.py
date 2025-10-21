a0a8ac97c8279fcd79274b59a328e2aad1969389
TJRoots <tjroots@hotmail.co.uk>
2025-10-21 20:06:32 +0100

    added a filepath argument

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-
+import sys
 from stats import count_words, count_characters, sorted_character_count
-    
-word_count = count_words("books/frankenstein.txt")
-character_count = count_characters("books/frankenstein.txt")
 
-print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+if len(sys.argv) <= 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+word_count = count_words(sys.argv[1])
+character_count = count_characters(sys.argv[1])
+
+print("============ BOOKBOT ============\nAnalyzing book found at "+str(sys.argv[1])+"...\n----------- Word Count ----------")
 print("Found " + str(word_count) + " total words")
 #print(character_count)
 print("--------- Character Count -------")
-for char, count in sorted_character_count("books/frankenstein.txt"):
+for char, count in sorted_character_count(sys.argv[1]):
     if char.isalpha():
         print(char + ": " + str(count))
 print("============= END ===============")
